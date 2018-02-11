887c7e93bfff6b79b61647dc25a77f803834d6a0
Lennart Buhl <git@hackmate.de>
2018-02-11 17:23:36 +0100

    hardcode

--- run-geth-client-bootnode.sh
+++ run-geth-client-bootnode.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
-if [ -z "$1" ]; then
-    echo "Please call with bootnode as paramter"
-    exit
-fi
-
 geth \
 	--datadir spacedata \
 	--networkid 1337 \
 	--nodiscover \
 	--rpc --rpcaddr 127.0.0.1 --rpcport 8545 \
-	#--rpcapi "web3,eth" --rpccorsdomain="chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn" --rpccorsdomain="http://localhost:8000" \
-	--bootnodes ${1}
-	#console
+	--bootnodes "enode://723128f962572909eb0f794580452249193ace7e2f56d48fb4b890a0b51f5a3d82e99f6f7e39a9c6bae4344de2bc56640d2669f284a86f391b46502d99605b34@192.168.200.144:30303"
